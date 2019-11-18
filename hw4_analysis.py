61d43c6cc2004e89099fcaa152c57a217e9fab44
Timmy Wu <timmy@stringbean.tech>
2019-11-18 09:00:41 -0500

    feat: decision tree: 90%

--- hw4_analysis.py
+++ hw4_analysis.py
@@ -1,28 +1,42 @@
 import pandas as pd
 import os
 import numpy as np
+import unicodedata
 
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+# from sklearn import datasets, linear_model
+# from sklearn.metrics import mean_squared_error, r2_score
 
-# from sklearn import datasets
-# from sklearn.feature_selection import RFE
-# from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import KFold
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.linear_model import LinearRegression
-# from sklearn.linear_model import Lasso
-# from sklearn.linear_model import ElasticNet
-# from sklearn.tree import DecisionTreeRegressor
-# from sklearn.neighbors import KNeighborsRegressor
-# from sklearn.ensemble import GradientBoostingRegressor
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Integer
+# 'age_r', 'v231', 'v272', 'earnmthbonusppp', 'readytolearn', 'icthome',
+#       'ictwork', 'influence', 'planning', 'readhome', 'readwork', 'taskdisc',
+#       'writhome', 'writwork'
+#
+# replace (ordinal or binary categories)
+# v200, fnfe12jr, edcat7
+#
+# one hot
+# ["v31", "ctryqual"]
+#
 
 
 # Init Dataframes
 df = pd.read_csv("hw4-trainingset-pw2440.csv")
 # Columns (50,172,255,256,257,258,268,280,376) have mixed types.
+print(df.select_dtypes(include=["float64", "int64"]))
 
 selected_features = [
     "age_r",
@@ -30,9 +44,15 @@ selected_features = [
     "v231",
     "v272",
     "v31",
-    "ctryqual",
+    # "ctryqual",
+    "yrsqual",
     "earnmthbonusppp",
-    "fnfe12jr",
+    "earnhrbonusppp",
+   "nfe12jr",
+   "nfe12njr",
+   "fnfe12jr",
+   "fnfaet12jr",
+   "fnfaet12njr",
     "readytolearn",
     "icthome",
     "ictwork",
@@ -47,71 +67,115 @@ selected_features = [
 ]
 
 cleanup = {
-    "v200": {
-        "High school": 2.0,
-        "Above high school": 3.0,
-        "Less than high school": 1.0,
-    },
+    # "v200": {
+    #     "High school": 2.0,
+    #     "Above high school": 3.0,
+    #     "Less than high school": 1.0,
+    #     "Not definable": np.nan,
+    # },
     "fnfe12jr": {
         "Participated in FE or NFE for JR reasons": 1.0,
         "Did not participate in FE or NFE for JR reasons": 0.0,
     },
+    "nfe12jr": {
+        "Participated in NFE for JR reasons": 1,
+        "Did not participate in NFE for JR reasons": 0,
+    },
+    "nfe12njr": {
+        "Did not participate in NFE for NJR reasons": 0,
+        "Participated in NFE for NJR reasons": 1,
+    },
+    "fnfaet12jr": {
+        "Participated in formal or non-formal AET for JR reasons": 1,
+        "Did not participate in formal or non-formal AET for JR reasons" : 0,
+    },
+    "fnfaet12njr":{
+        "Did not participate in formal or non-formal AET for non JR reasons": 0,
+        "Participated in formal or non-formal AET for non JR reasons" : 1,
+    },
+    "edcat7": {
+        "Primary or less (ISCED 1 or less)": 1.0,
+        "Lower secondary (ISCED 2, ISCED 3C short)": 2.0,
+        "Upper secondary (ISCED 3A-B, C long)" : 3.0,
+        "Post-secondary, non-tertiary (ISCED 4A-B-C)" : 4.0,
+        "Tertiary  bachelor degree (ISCED 5A)" : 5.0,
+        "Tertiary - bachelor/master/research degree (ISCED 5A/6)" : 6.0,
+        "Tertiary  master/research degree (ISCED 5A/6)" : 7.0,
+        "Tertiary  professional degree (ISCED 5B)" : 8.0,
+    }
 }
 
-df1 = df[selected_features]
+df1 = df[selected_features].copy()
+df1['edcat7'] = df1['edcat7'].str.replace('[^\x00-\x7F]','')
 int_df = df1.select_dtypes(include=["float64", "int64"]).copy()
-# Integer
-# 'age_r', 'v231', 'v272', 'earnmthbonusppp', 'readytolearn', 'icthome',
-#       'ictwork', 'influence', 'planning', 'readhome', 'readwork', 'taskdisc',
-#       'writhome', 'writwork'
-#
-# replace (ordinal or binary categories)
-# v200, fnfe12jr
-#
-# one hot
-# ["v31", "ctryqual"]
-#
 
 # fill mean in integer columns
 int_df.fillna(int_df.mean(), inplace=True)
 
-# object columns setup
-cat_obj = ["v31", "ctryqual", "v200", "fnfe12jr", "edcat7"]
+ord_cat = ["nfe12jr", "nfe12njr", "fnfaet12jr", "fnfaet12njr", "fnfe12jr"]
+#cat_obj = ["v31", "ctryqual", "edcat7", "v200"]
+cat_obj = ["v31", "v200", "edcat7"] + ord_cat
 obj_df = df1.select_dtypes(include=["object"]).copy()
-# fill in mode
+
+obj_df.replace(cleanup, inplace=True)
+
+# fill in median
+for i in ord_cat:
+    obj_df[i].fillna(obj_df[i].median(), inplace=True)
+
+# # fill in mode
 for i in cat_obj:
     obj_df[i].fillna(obj_df[i].mode()[0], inplace=True)
 
-obj_df.replace(cleanup, inplace=True)
+# obj_df = obj_df.astype({'edcat7': 'int32', 'v200': 'int32', 'fnfe12jr': 'int32'})
+# obj_df = obj_df.astype({'v200': 'int32', 'fnfe12jr': 'int32'})
+
 # one hot
 obj_df = pd.get_dummies(obj_df, columns=cat_obj)
 df_init = pd.concat([int_df, obj_df], axis=1)
+
+# df_init = int_df
+
+# print(df_init)
 target = df["job_performance"]
+print(target.median())
 
-# print(df_init.isnull().sum())
+# train and test set
+# x_train = df_init[:-4000]
+# x_test = df_init[-4000:]
 
-df_init_x_train = df_init[:-4000]
-df_init_x_test = df_init[-4000:]
+# y_train = target[:-4000]
+# y_test = target[-4000:]
 
-df_init_y_train = target[:-4000]
-df_init_y_test = target[-4000:]
+# # Create linear regression object
+# regr = linear_model.LinearRegression()
 
+# # Train the model using the training sets
+# regr.fit(df_init_x_train, df_init_y_train)
 
-# Create linear regression object
-regr = linear_model.LinearRegression()
+# # Make predictions using the testing set
+# df_y_pred = regr.predict(df_init_x_test)
 
-# Train the model using the training sets
-regr.fit(df_init_x_train, df_init_y_train)
+# # The coefficients
+# print("Coefficients: \n", regr.coef_)
+# # The mean squared error
+# print("Mean squared error: %.2f" % mean_squared_error(df_init_y_test, df_y_pred))
+# # Explained variance score: 1 is perfect prediction
+# print("Variance score: %.2f" % r2_score(df_init_y_test, df_y_pred))
 
-# Make predictions using the testing set
-df_y_pred = regr.predict(df_init_x_test)
+##### gradient boosted alg #######
+# from sklearn import ensemble
+# from sklearn.metrics import mean_squared_error
 
-# The coefficients
-print("Coefficients: \n", regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(df_init_y_test, df_y_pred))
-# Explained variance score: 1 is perfect prediction
-print("Variance score: %.2f" % r2_score(df_init_y_test, df_y_pred))
+# # Fit regression model
+# params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2,
+#           'learning_rate': 0.01, 'loss': 'ls'}
+# clf = ensemble.GradientBoostingRegressor(**params)
+
+# clf.fit(x_train, y_train)
+# mse = mean_squared_error(y_test, clf.predict(x_test))
+# print("MSE: %.4f" % mse)
+####################################
 
 ######
 # pipelines = []
@@ -146,58 +210,11 @@ print("Variance score: %.2f" % r2_score(df_init_y_test, df_y_pred))
 # results = []
 # names = []
 # for name, model in pipelines:
-#     kfold = KFold(n_splits=10, random_state=21)
+#     kfold = KFold(n_splits=10, random_state=15)
 #     cv_results = cross_val_score(
-#         model, df_init, target, cv=kfold, scoring="neg_mean_squared_error"
+#         model, x_train, y_train, cv=kfold, scoring="neg_mean_squared_error"
 #     )
 #     results.append(cv_results)
 #     names.append(name)
 #     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 #     print(msg)
-
-# count = raw_train.apply(lambda x: x.count(), axis=0)
-# print(raw_train.append(count, ignore_index=True))
-
-# count2 = df.apply(lambda col: col.isnull().sum() > 10000)
-
-
-# df = df.drop(df.columns[df.apply(lambda x: x.isnull().sum() > 18000)], axis=1)
-# print(df.shape)
-# print(df.dtypes)
-# g = df.columns.to_series().groupby(df.dtypes).groups
-# print(g)
-
-# obj_df = df.select_dtypes(include=['float64', 'int64']).copy()
-# print(obj_df.columns)
-
-# def mask_wle_ca(df, column_name):
-#     mask20 = df[column_name] == 'Lowest to 20%'
-#     mask40 = df[column_name] == 'More than 20% to 40%'
-#     mask60 = df[column_name] == 'More than 40% to 60%'
-#     mask80 = df[column_name] == 'More than 60% to 80%'
-#     mask100 = df[column_name] == 'More than 80%'
-
-#     df.loc[mask20, column_name] = 1
-#     df.loc[mask40, column_name] = 2
-#     df.loc[mask60, column_name] = 3
-#     df.loc[mask80, column_name] = 4
-#     df.loc[mask100, column_name] = 5
-
-# def binary_gender(df, column_name):
-#     mask_male = df[column_name] == 'Male'
-#     mask_female = df[column_name] == 'Female'
-
-#     df.loc[mask_male, column_name] = 0
-#     df.loc[maks_female, column_name] = 1
-
-# cleanup_wle_ca = { wle_ca_df:
-
-# wle_ca_df = df.filter(regex="wle_ca$", axis=1).copy()
-# wle_ca_list = list(df.filter(regex="wle_ca$", axis=1).columns)
-
-# df_2 = df.copy()
-# for i in wle_ca_list:
-# 	mask_wle_ca(df_2, i)
-
-# print(df_2)
-
